6fb33b5e77c1f32bcb9936f16ce0091ee5436c83
Nathan Kulow <nathan.kulo@meijer.com>
2023-01-09 17:03:29 -0500

    add unit tests

--- validate-parentheses.test.js
+++ validate-parentheses.test.js
@@ -1,7 +1,12 @@
 import { validateParentheses } from "./validate-parentheses.js";
 
 describe ('validate LISP parentheses', () => {
+    
     test('should return true for valid LISP code', () => {
-        
+        expect(validateParentheses('(valid () test)')).toEqual(true);
+    });
+
+    test('should return false for invalid LISP code', () => {
+        expect(validateParentheses('(valid (() test)')).toEqual(false);
     });
 });
